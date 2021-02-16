92411bbf21b27f793bf5ab52502c31df77317aa5
johnhumms <77086782+johnhumms@users.noreply.github.com>
2021-02-15 21:18:19 -0600

    Added characters. Set up user picking password length.
    Added uppercase, lowercase, and special characters. Added user prompt to enter in password length.

--- script.js
+++ script.js
@@ -1,22 +1,60 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Password Length
-//Include Special Characters
 
-let passwordLength = 10;
-let includeSpecial = true;
-let includeLowercase = true;
-let includeUppercase = false;
+function generatePassword(
+  passwordLength, 
+  includeSpecial, 
+  includeLowercase, 
+  includeUppercase
+  ) {
 
-function generatePassword() {
-  return "Test"
+    let characterOptions = [];
+    
+    let lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+    let uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]; 
+    let special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+"];
+
+    //concat combines arrays
+    if (includeSpecial) {
+      characterOptions = characterOptions.concat(special)
+    }
+
+    if (includeUppercase) {
+      characterOptions = characterOptions.concat(uppercase)
+    }
+
+    if (includeLowercase) {
+      characterOptions =characterOptions.concat(lowercase)
+    }
+
+    let res = ""
+
+    for (i=0; i<passwordLength; i++) {
+      let randomIndex = Math.floor(Math.random() * characterOptions.length)
+      res += characterOptions[randomIndex]
+    }
+    return res
 }
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+    let passwordLength = window.prompt("Enter desired passoword length.");
+    // how do I prompt a yes or no to user??
+    let includeSpecial = true;
+    let includeLowercase = true;
+    let includeUppercase = true;
+
+    //validate options?
+
+    var password = generatePassword(
+      passwordLength,
+      includeSpecial,
+      includeLowercase,
+      includeUppercase,
+      );
+
+    var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
